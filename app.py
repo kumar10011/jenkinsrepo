3ba74bb4d056863cf7b03d9470a233813ccb8d91
Ubuntu <ubuntu@ip-172-31-26-242.ec2.internal>
2025-10-10 09:42:34 +0000

    added app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Jenkins Flask Pipeline!"
+    return "Hello Bro Iam Anjani from Jenkins Flask Pipeline!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=3000)
